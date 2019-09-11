2f5ae58595d4071cc277504a08e0e8e2b16807db
Viking Den <vikingden@live.com>
2019-09-11 22:14:19 +0800

    add first fizz test case

---
+++ src/test/java/com/pingan/test/GameTest.java
@@ -0,0 +1,15 @@
+package com.pingan.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+public class GameTest {
+
+    @Test
+    public void should_return_fizz_when_just_a_multiple_of_the_first_random(){
+        List<GameRule> gameRules = new ArrayList<>() ;
+        assertEquals(Student.countOff(3, gameRules), "Fizz");
+    }
+}
