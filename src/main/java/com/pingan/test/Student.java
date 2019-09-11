ca07e17bea3021ae8a4df3dd7dd6b6e2a25b3c29
Viking Den <vikingden@live.com>
2019-09-11 23:12:51 +0800

    finish all simple tasks

--- src/main/java/com/pingan/test/Student.java
+++ src/main/java/com/pingan/test/Student.java
@@ -5,8 +5,12 @@ import java.util.List;
 public class Student {
 
     public static String countOff(Integer num, List<GameRule> gameRules) {
-        if (num % gameRules.get(0).getNumber() == 0){
+        if ( num % gameRules.get(0).getNumber() == 0){
             return gameRules.get(0).getTerm() ;
+        } else if ( num % gameRules.get(1).getNumber() == 0){
+            return gameRules.get(1).getTerm() ;
+        } else if ( num % gameRules.get(2).getNumber() == 0){
+            return gameRules.get(2).getTerm() ;
         }
         return num.toString() ;
     }
--- src/test/java/com/pingan/test/GameTest.java
+++ src/test/java/com/pingan/test/GameTest.java
@@ -1,5 +1,6 @@
 package com.pingan.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,12 +9,35 @@ import java.util.List;
 
 public class GameTest {
 
-    @Test
-    public void should_return_fizz_when_just_a_multiple_of_the_first_random(){
-        List<GameRule> gameRules = new ArrayList<>() ;
+    private List<GameRule> gameRules ;
+
+    @BeforeEach
+    void setUp(){
+        gameRules = new ArrayList<>() ;
         gameRules.add(new GameRule(3, "Fizz")) ;
         gameRules.add(new GameRule(5, "Buzz")) ;
         gameRules.add(new GameRule(7, "Whizz")) ;
+    }
+
+    @Test
+    void should_return_1_when_mismatch_any_random_number(){
+        assertEquals(Student.countOff(1, gameRules), "1");
+    }
+
+    @Test
+    void should_return_fizz_when_just_a_multiple_of_the_second_random(){
         assertEquals(Student.countOff(3, gameRules), "Fizz");
     }
+
+    @Test
+    void should_return_buzz_when_just_a_multiple_of_the_first_random(){
+        assertEquals(Student.countOff(5, gameRules), "Buzz");
+    }
+
+    @Test
+    void should_return_whizz_when_just_a_multiple_of_the_third_random(){
+        assertEquals(Student.countOff(7, gameRules), "Whizz");
+    }
+
+
 }
